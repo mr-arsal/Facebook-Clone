2364a3ee4488b81f96c7a00f31f682ef440a5ac1
Mr-Arsal <an24547@gmail.com>
2022-09-18 13:51:01 +0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      xsm: '320px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      spacing: {
+        13: '13px'
+      },
+    },
+  },
+  plugins: [],
+}
